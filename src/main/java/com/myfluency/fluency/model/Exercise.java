7b489fbd1af1d8f0a4b11c9c940c0aad12bcf2e9
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-28 20:49:25 -0300

    Entity Exercise

--- src/main/java/com/myfluency/fluency/model/Exercise.java
+++ src/main/java/com/myfluency/fluency/model/Exercise.java
@@ -1,4 +1,27 @@
 package com.myfluency.fluency.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String question;
+
+    @ElementCollection
+    private List<String> options;
+    private String correctAnswer;
+    private String difficulty;
 }
