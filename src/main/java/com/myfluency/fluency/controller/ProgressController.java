2e72e90082ee85ac22379ff56ea18cbeaf656dfb
Joshua Dias <joshuadiasbf@gmail.com>
2024-12-28 21:33:38 -0300

    ProgressController

--- src/main/java/com/myfluency/fluency/controller/ProgressController.java
+++ src/main/java/com/myfluency/fluency/controller/ProgressController.java
@@ -1,12 +1,26 @@
 package com.myfluency.fluency.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.myfluency.fluency.model.Progress;
+import com.myfluency.fluency.service.ProgressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/progress")
 public class ProgressController {
 
+    @Autowired
+    private ProgressService progressService;
 
+    @GetMapping("/{userId}")
+    public List<Progress> getProgressByUser(@PathVariable Long userId) {
+        return progressService.getProgressByUser(userId);
+    }
 
+    @PostMapping
+    public Progress updatedProgress(@RequestBody Progress progress) {
+        return progressService.updatedProgress(progress);
+    }
 }
